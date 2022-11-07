62cc0383dcf3b40bddd30b7f76e79b6a2587d1e2
lyongaming <owlsmouth@gmail.com>
2022-11-07 05:20:57 -0400

    first commit

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const PORT = process.env.PORT || "8080";
+const app = express();
+
+app.use(cors());
+
+app.listen(PORT, () => console.log(`SERVER CONNECTED TO ${PORT}`))
